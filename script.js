8ab13d03af397b0c15f1cdbfb67cc191c9b3200d
EdmondV <edmond.kin22@gmail.com>
2017-04-11 16:36:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(
+    function(){
+       $('#button').click(
+           function(){
+               var new_task = $('input[name=TaskList]').val();
+               $('ol').append('<li>' + new_task + '</li>');
+      	});
+       
+       $("input[name=TaskList]").keyup(function(event){
+          if(event.keyCode == 13){
+            $("#button").click();
+          }         
+      	});
+      
+      $(document).on('dblclick','li', function(){
+        $(this).toggleClass('strike').fadeOut('slow');    
+      	});
+      
+      $('input').focus(function() {
+        $(this).val('');
+      	});
+      
+      $('ol').sortable();  
+
+      $('#mrProper').click(
+           function(){
+               $('ol').empty();
+      	});
+      
+    	}
+);
+
