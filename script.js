5034bb700f07faa55c110fd6757399f614f6dfca
EdmondV <edmond.kin22@gmail.com>
2017-04-11 20:52:44 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(
     function(){
        $('#button').click(
@@ -31,37 +30,3 @@ $(document).ready(
     	}
 );
 
-=======
-$(document).ready(
-    function(){
-       $('#button').click(
-           function(){
-               var new_task = $('input[name=TaskList]').val();
-               $('ol').append('<li>' + new_task + '</li>');
-      	});
-       
-       $("input[name=TaskList]").keyup(function(event){
-          if(event.keyCode == 13){
-            $("#button").click();
-          }         
-      	});
-      
-      $(document).on('dblclick','li', function(){
-        $(this).toggleClass('strike').fadeOut('slow');    
-      	});
-      
-      $('input').focus(function() {
-        $(this).val('');
-      	});
-      
-      $('ol').sortable();  
-
-      $('#mrProper').click(
-           function(){
-               $('ol').empty();
-      	});
-      
-    	}
-);
-
->>>>>>> origin/master
